b232c45f5935b026cdfe6e8bdad64a9746a95be5
Axel <109250820+AxelG28@users.noreply.github.com>
2022-07-13 22:35:45 -0300

    carlos menem

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -63,6 +63,8 @@ function sonIguales(x, y) {
   
 }
 
+//pingoloco
+
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
